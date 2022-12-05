69b8aaee35e6ca6744a83e27ea49c2849b3df17c
Roma Antoliak <104556918+roma-cwby@users.noreply.github.com>
2022-12-05 16:15:47 +0200

    fix

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -3,9 +3,6 @@ import { Feedback } from './FeedbackOptions.styled';
 export const FeedbackOptions = ({ options, click }) => {
   return (
     <Feedback>
-      {/* <button onClick={()=>{click("g")}} type="button">Good</button>
-            <button onClick={()=>{click("n")}} type="button">Neutral</button>
-            <button onClick={()=>{click()}} type="button">Bad</button> */}
       {options.map(option => (
         <button
           key={option}
