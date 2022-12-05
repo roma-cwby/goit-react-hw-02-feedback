5a6c3fce356f08770d8331ad86294047fd4310e7
Roma Antoliak <104556918+roma-cwby@users.noreply.github.com>
2022-12-05 16:14:43 +0200

    refct

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,51 +1,53 @@
-import {Component} from "react";
-import { Section } from "components/Section/Section";
-import { FeedbackOptions } from "components/FeedbackOptions/FeedbackOptions";
-import { Statistics } from "components/Statistics/Statistics";
-import { Notification } from "components/Notification/Notification";
+import { Component } from 'react';
+import { Section } from 'components/Section/Section';
+import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
+import { Statistics } from 'components/Statistics/Statistics';
+import { Notification } from 'components/Notification/Notification';
 
 export class App extends Component {
   state = {
     good: 0,
     neutral: 0,
-    bad: 0
-  }
+    bad: 0,
+  };
 
   countTotalFeedback = () => {
-    return this.state.good + this.state.neutral + this.state.bad;
-  }
+    return Object.values(this.state).reduce((acc, value) => acc + value, 0);
+  };
 
   countPositiveFeedbackPercentage = () => {
-    return Math.round(this.state.good * 100 / this.countTotalFeedback());
-  }
-  
-  incrementFeedback = (type = null) => {
-    this.setState(prevState => {
-      if(type === "g")
-      return {
-        good: prevState.good + 1,
-      };
-      else if (type === "n")
-      return {
-        neutral: prevState.neutral + 1,
-      };
-      else 
-      return {
-        bad: prevState.bad + 1,
-      };
-    });
-  }
+    return Math.round((this.state.good * 100) / this.countTotalFeedback());
+  };
 
-  render(){
-    const {good, neutral, bad} = this.state;
+  incrementFeedback = type => {
+    this.setState(prevState => ({ [type]: prevState[type] + 1 }));
+  };
 
-    return <Section>
-      <Section title="Please leave feedback">
-      <FeedbackOptions click={this.incrementFeedback}/>
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <Section>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            click={this.incrementFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
       </Section>
-      <Section title="Statistics">
-      {this.countTotalFeedback() ? <Statistics good={good} neutral={neutral} bad={bad} total={this.countTotalFeedback()} positivePercentage={this.countPositiveFeedbackPercentage()}/> : <Notification message="There is no feedback"/>}
-      </Section>
-      </Section>
+    );
   }
-};
+}
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,11 +1,23 @@
-import { Feedback } from "./FeedbackOptions.styled";
+import { Feedback } from './FeedbackOptions.styled';
 
-export const FeedbackOptions = ({click}) => {
-    return(
-        <Feedback>
-            <button onClick={()=>{click("g")}} type="button">Good</button>
+export const FeedbackOptions = ({ options, click }) => {
+  return (
+    <Feedback>
+      {/* <button onClick={()=>{click("g")}} type="button">Good</button>
             <button onClick={()=>{click("n")}} type="button">Neutral</button>
-            <button onClick={()=>{click()}} type="button">Bad</button>
-        </Feedback>
-    );
-}
+            <button onClick={()=>{click()}} type="button">Bad</button> */}
+      {options.map(option => (
+        <button
+          key={option}
+          name={option}
+          onClick={e => {
+            click(e.target.name);
+          }}
+          type="button"
+        >
+          {option}
+        </button>
+      ))}
+    </Feedback>
+  );
+};
